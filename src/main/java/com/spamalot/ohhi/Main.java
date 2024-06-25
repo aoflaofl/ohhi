0b6d1e812a7dbd5ca677951491bf359b47e0a554
Gene Johannsen <aofl@yahoo.com>
2024-06-25 08:32:55 -0700

    Some refactoring, add logging

--- src/main/java/com/spamalot/ohhi/Main.java
+++ src/main/java/com/spamalot/ohhi/Main.java
@@ -17,29 +17,25 @@ public final class Main {
   /**
    * Really, start here.
    * 
-   * @param args
-   *          Arguments to the program
+   * @param args Arguments to the program
    */
   public static void main(final String[] args) {
     PuzzleBoard puzzleBoard = new PuzzleBoard(8);
-
-    puzzleBoard.setCell(CellValue.RED, 0, 0);
-    puzzleBoard.setCell(CellValue.RED, 0, 5);
-    puzzleBoard.setCell(CellValue.RED, 1, 7);
-    puzzleBoard.setCell(CellValue.RED, 2, 0);
-    puzzleBoard.setCell(CellValue.BLUE, 2, 3);
-    puzzleBoard.setCell(CellValue.RED, 3, 2);
-    puzzleBoard.setCell(CellValue.RED, 3, 4);
-    puzzleBoard.setCell(CellValue.RED, 3, 7);
-    puzzleBoard.setCell(CellValue.RED, 5, 0);
-    puzzleBoard.setCell(CellValue.RED, 5, 3);
-    puzzleBoard.setCell(CellValue.RED, 5, 5);
-    puzzleBoard.setCell(CellValue.RED, 5, 7);
-    puzzleBoard.setCell(CellValue.RED, 6, 5);
-    puzzleBoard.setCell(CellValue.RED, 6, 7);
-    puzzleBoard.setCell(CellValue.RED, 7, 0);
-    puzzleBoard.setCell(CellValue.RED, 7, 2);
-
+    setInitialCells(puzzleBoard);
     PuzzleSolver.solve(puzzleBoard);
+  }
+
+  private static void setInitialCells(PuzzleBoard puzzleBoard) {
+    int[][] redCells = { { 0, 0 }, { 0, 5 }, { 1, 7 }, { 2, 0 }, { 3, 2 }, { 3, 4 }, { 3, 7 }, { 5, 0 }, { 5, 3 },
+        { 5, 5 }, { 5, 7 }, { 6, 5 }, { 6, 7 }, { 7, 0 }, { 7, 2 } };
+    int[][] blueCells = { { 2, 3 } };
+
+    for (int[] cell : redCells) {
+      puzzleBoard.setCell(CellValue.RED, cell[0], cell[1]);
+    }
+
+    for (int[] cell : blueCells) {
+      puzzleBoard.setCell(CellValue.BLUE, cell[0], cell[1]);
+    }
   }
 }
--- src/main/java/com/spamalot/ohhi/PuzzleSolver.java
+++ src/main/java/com/spamalot/ohhi/PuzzleSolver.java
@@ -1,91 +1,99 @@
-
 package com.spamalot.ohhi;
 
 import java.util.HashMap;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 class PuzzleSolver {
+  private static final Logger LOGGER = LoggerFactory.getLogger(PuzzleSolver.class);
 
-	private PuzzleSolver() {
-	}
+  private PuzzleSolver() {
+  }
 
-	public static void solve(PuzzleBoard puzzleBoard) {
-		processCellGroups(puzzleBoard.getRowCellGroups());
-		processCellGroups(puzzleBoard.getColumnCellGroups());
+  public static void solve(PuzzleBoard puzzleBoard) {
+    processCellGroups(puzzleBoard.getRowCellGroups());
+    processCellGroups(puzzleBoard.getColumnCellGroups());
 
-		while (solveThree(puzzleBoard.getRowCellGroups()) || solveThree(puzzleBoard.getColumnCellGroups())) {
-		}
+    while (solveThree(puzzleBoard.getRowCellGroups()) || solveThree(puzzleBoard.getColumnCellGroups())) {
+    }
 
-		while (evenNumber(puzzleBoard)) {
-		}
+    while (evenNumber(puzzleBoard)) {
+    }
 
-		while (solveThree(puzzleBoard.getRowCellGroups()) || solveThree(puzzleBoard.getColumnCellGroups())) {
-		}
+    while (solveThree(puzzleBoard.getRowCellGroups()) || solveThree(puzzleBoard.getColumnCellGroups())) {
+    }
 
-		evenNumber(puzzleBoard);
-		puzzleBoard.processGroups();
-	}
+    evenNumber(puzzleBoard);
+    puzzleBoard.processGroups();
+  }
 
-	private static boolean solveThree(CellGroup[] group) {
-		boolean changeMade = false;
-		for (CellGroup cg : group) {
-			while (solveThreeInARow(cg)) {
-				changeMade = true;
-			}
-		}
-		return changeMade;
-	}
+  private static boolean solveThree(CellGroup[] group) {
+    boolean changeMade = false;
+    for (CellGroup cg : group) {
+      while (solveThreeInARow(cg)) {
+        LOGGER.info("Solved Three in a row.");
+        changeMade = true;
+      }
+    }
+    return changeMade;
+  }
 
-	private static boolean solveThreeInARow(CellGroup cellGroup) {
-		boolean changeMade = false;
-		int size = cellGroup.getSize();
-		for (int i = 0; i < size - 2; i++) {
-			if (fixThreeInARow(cellGroup.getCell(i), cellGroup.getCell(i + 1), cellGroup.getCell(i + 2))) {
-				changeMade = true;
-			}
-		}
-		return changeMade;
-	}
+  private static boolean solveThreeInARow(CellGroup cellGroup) {
+    boolean changeMade = false;
+    int size = cellGroup.getSize();
+    for (int i = 0; i < size - 2; i++) {
+      if (fixThreeInARow(cellGroup, i)) {
+        changeMade = true;
+      }
+    }
+    return changeMade;
+  }
 
-	private static boolean fixThreeInARow(Cell c1, Cell c2, Cell c3) {
-		if (c1.isEmpty() && c2.isSameColor(c3)) {
-			c1.setValue(c2.getValue().opposite());
-			return true;
-		}
-		if (c2.isEmpty() && c1.isSameColor(c3)) {
-			c2.setValue(c1.getValue().opposite());
-			return true;
-		}
-		if (c3.isEmpty() && c1.isSameColor(c2)) {
-			c3.setValue(c1.getValue().opposite());
-			return true;
-		}
-		return false;
-	}
+  private static boolean fixThreeInARow(CellGroup cellGroup, int index) {
+    Cell c1 = cellGroup.getCell(index);
+    Cell c2 = cellGroup.getCell(index + 1);
+    Cell c3 = cellGroup.getCell(index + 2);
 
-	private static boolean evenNumber(PuzzleBoard p) {
-		boolean changeMade = false;
-		for (int i = 0; i < p.getSize(); i++) {
-			changeMade = evenNumber(p.getRowCellGroups()[i]) || changeMade;
-			changeMade = evenNumber(p.getColumnCellGroups()[i]) || changeMade;
-		}
-		return changeMade;
-	}
+    if (c1.isEmpty() && c2.isSameColor(c3)) {
+      c1.setValue(c2.getValue().opposite());
+      return true;
+    }
+    if (c2.isEmpty() && c1.isSameColor(c3)) {
+      c2.setValue(c1.getValue().opposite());
+      return true;
+    }
+    if (c3.isEmpty() && c1.isSameColor(c2)) {
+      c3.setValue(c1.getValue().opposite());
+      return true;
+    }
+    return false;
+  }
 
-	private static boolean evenNumber(CellGroup cg) {
-		int fillSize = cg.getSize() / 2;
-		HashMap<CellValue, Integer> m = cg.getCountMap();
-		for (CellValue color : m.keySet()) {
-			if (m.get(color) == fillSize) {
-				cg.fillEmptyWith(color.opposite());
-				return true;
-			}
-		}
-		return false;
-	}
+  private static boolean evenNumber(PuzzleBoard p) {
+    boolean changeMade = false;
+    for (int i = 0; i < p.getSize(); i++) {
+      changeMade = evenNumber(p.getRowCellGroups()[i]) || changeMade;
+      changeMade = evenNumber(p.getColumnCellGroups()[i]) || changeMade;
+    }
+    return changeMade;
+  }
 
-	private static void processCellGroups(CellGroup[] groups) {
-		for (CellGroup cg : groups) {
-			System.out.println(cg);
-		}
-	}
+  private static boolean evenNumber(CellGroup cg) {
+    int fillSize = cg.getSize() / 2;
+    HashMap<CellValue, Integer> m = cg.getCountMap();
+    for (CellValue color : m.keySet()) {
+      if (m.get(color) == fillSize) {
+        cg.fillEmptyWith(color.opposite());
+        return true;
+      }
+    }
+    return false;
+  }
+
+  private static void processCellGroups(CellGroup[] groups) {
+    for (CellGroup cg : groups) {
+      System.out.println(cg);
+    }
+  }
 }
